8e7ac3fc2ae92e42873eb25b1b6ffdcbe220f026
Shivam Arora <70646279+shivam2813@users.noreply.github.com>
2023-12-02 13:44:16 +0530

    Backend made

--- server/main.py
+++ server/main.py
@@ -1,4 +1,44 @@
-from flask import Flask 
-
+from flask import Flask ,jsonify
 app=Flask(__name__)
+
+itemList = [{
+        "itemName" : "Rice",
+        "unitPrice" : 24,
+        "quantity" : 5,
+        "isPurchased" : False
+    },
+    {
+        "itemName" : "Potato",
+        "unitPrice" : 30,
+        "quantity" : 4,
+        "isPurchased" : False
+    },
+    {
+        "itemName" : "Soap",
+        "unitPrice" : 60,
+        "quantity" : 2,
+        "isPurchased" : False
+    },
+    {
+        "itemName" : "Maggie",
+        "unitPrice" : 15,
+        "quantity" : 4,
+        "isPurchased" : False
+    },
+    {
+        "itemName" : "Apple",
+        "unitPrice" : 200,
+        "quantity" : 1,
+        "isPurchased" : False
+    }]
+
+
+@app.route('/')
+def itemdata():
+    try:
+        return jsonify(success="y",data=itemList)
+    except:
+        return jsonify(success="n")
+    
+
 
